d00addb4ce586ba1ac5773f8b143d4164532926c
Augusto Batista <augustobatista@Augustos-MacBook-Pro-3.local>
2021-01-07 00:15:11 -0500

    implemeneted shipping screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPIMG_ADRESS,
+} from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -26,4 +30,13 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   });
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAdress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPIMG_ADRESS,
+    payload: data,
+  });
+
+  localStorage.setItem("shippingAdress", JSON.stringify(data));
 };
