7d8777589642af95b56fb9e969916a0fc942dd5c
Augusto Batista <augustobatista@Augustos-MacBook-Pro-3.local>
2021-01-08 00:37:06 -0500

    implemented order controller and routes in backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
